39f435d97760d1c14f8c85bca42ab8a73473dffe
Alex Ward <alxwrd@googlemail.com>
2024-11-15 17:52:11 +0000

    add missing tailwind config updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+  safelist: [
+    {
+      pattern: /(^bg-|^border-)/,
+    },
+  ],
   theme: {
     extend: {
       keyframes: {
@@ -8,9 +13,17 @@ export default {
           '0%, 100%': { transform: 'rotate(-2deg)' },
           '50%': { transform: 'rotate(2deg)' },
         },
+        heartbeat: {
+          '0%': { transform: 'scale(1);' },
+          '14%': { transform: 'scale(1.3);' },
+          '28%': { transform: 'scale(1);' },
+          '42%': { transform: 'scale(1.3);' },
+          '70%': { transform: 'scale(1);' },
+        },
       },
       animation: {
         wiggle: 'wiggle 2s ease-in-out infinite',
+        heartbeat: 'heartbeat 1s',
       },
     },
   },
