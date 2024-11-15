323fa2acecd0a4c59a58c9e92853c4bcfc393439
Alex Ward <alxwrd@googlemail.com>
2024-11-15 17:51:19 +0000

    update CategoryItem to allow fields to be editable

--- src/components/CategoryItem.vue
+++ src/components/CategoryItem.vue
@@ -11,21 +11,30 @@ defineProps<{
 <script lang="ts"></script>
 
 <template>
-  <div class="bg-zinc-700 p-4 m-4 rounded w-80 text-white">
-    <h2 class="text-zinc-400 text-xl p-2">{{ title }}</h2>
-    <li v-for="(item, index) in options" :key="index" class="list-none px-6 py-3">
+  <div class="bg-zinc-800 p-4 m-4 rounded w-80 text-white border border-zinc-600">
+    <input class="bg-zinc-800 text-purple-300 text-xl p-2" :value="title" />
+    <div
+      v-for="(item, index) in options"
+      :key="index"
+      class="list-none px-6 py-3 flex flex-row overflow-hidden"
+    >
       <span :class="{ invisible: index !== pointer }" class="mr-2">
         <span v-if="item.state === 'discarded'">❌</span>
         <span v-else>👉</span>
       </span>
-      <span
-        :class="{
-          'text-teal-400 font-bold': item.state === 'chosen',
-          'line-through decoration-4 decoration-red-500': item.state === 'discarded',
-        }"
-      >
-        {{ item.title }}
-      </span>
-    </li>
+      <input
+        :class="[
+          {
+            'text-teal-500 font-bold text-xl': item.state === 'chosen',
+            'line-through decoration-2 decoration-pink-500 text-sm text-zinc-500':
+              item.state === 'discarded',
+          },
+          'bg-zinc-800',
+          'h-7',
+        ]"
+        :value="item.title"
+        :disabled="item.state !== 'waiting'"
+      />
+    </div>
   </div>
 </template>
